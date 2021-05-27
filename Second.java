363e024ad89d6efa521e97a14df8dc76a26318ce
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-28 00:23:20 +0200

    Update Second.java

--- Second.java
+++ Second.java
@@ -1,7 +1,7 @@
 package Better_string;
 
 public class Second {
- public static String  betterstring (String first,String second,Better_str obj) {
+ public static String  betterstring (String first,String second,BetterString obj) {
 	 
 	 if(obj.try_string(first , second))
 		 return first;
