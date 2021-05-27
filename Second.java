81be48c8e5e7ca7bc6c3a8906735953809f4ac2e
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-27 23:52:20 +0200

    Create Second.java

---
+++ Second.java
@@ -0,0 +1,10 @@
+package Better_string;
+
+public class Second {
+ public static String  betterstring (String first,String second,Better_str obj) {
+	 
+	 if(obj.try_string(first , second))
+		 return first;
+	 return second;
+ }
+}
